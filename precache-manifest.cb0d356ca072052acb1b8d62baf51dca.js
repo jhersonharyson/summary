b67752f61f8f17032c73e9fa889167b126f8e94e
Jherson Haryson <37131507+jhersonharyson@users.noreply.github.com>
2018-12-22 18:09:41 -0200

    criação do summary

---
+++ precache-manifest.cb0d356ca072052acb1b8d62baf51dca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d05d3a48e9dc7f29001c778e124019",
+    "url": "/static/media/foto.43d05d3a.jpeg"
+  },
+  {
+    "revision": "b2bc8d2dab42396822fa",
+    "url": "/static/css/main.1728f7c1.chunk.css"
+  },
+  {
+    "revision": "d479a29af347d17db9a69db4924e9a8b",
+    "url": "/static/media/ufpa.d479a29a.png"
+  },
+  {
+    "revision": "ef6f16df197c73cb03ff",
+    "url": "/static/js/1.ef6f16df.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2bc8d2dab42396822fa",
+    "url": "/static/js/main.b2bc8d2d.chunk.js"
+  },
+  {
+    "revision": "ccb32cfacdf0eb398df10868e45b8227",
+    "url": "/static/media/ciad.ccb32cfa.png"
+  },
+  {
+    "revision": "65378cbeaee93d533b4f4be1c48742ed",
+    "url": "/static/media/ec.65378cbe.png"
+  },
+  {
+    "revision": "aae0364c5f091c8063d0c1f2a642bcac",
+    "url": "/static/media/totvs.aae0364c.png"
+  },
+  {
+    "revision": "ef6f16df197c73cb03ff",
+    "url": "/static/css/1.d7a7ef04.chunk.css"
+  },
+  {
+    "revision": "cb353b5b9ea09d764a03debe28c8fb8b",
+    "url": "/index.html"
+  }
+];
